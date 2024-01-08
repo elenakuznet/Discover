c3a0d5c619ed84ed40d3166265a7b5ec6afce995
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-01-08 13:09:58 +0500

    add autocomplete

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import autoComplete from "@tarekraafat/autocomplete.js";
+import countryList from "./helpers/countryList";
+
+const autoCompleteJS = new autoComplete({ 
+    selector: "#autoCompleteCountry",
+    placeHolder: "e.g Bali, Indonesia",
+    data: {
+        src: countryList,
+        cache: true,
+    },
+    resultItem: {
+        highlight: true,
+    },
+    events: {
+        input: {
+            selection: (event) => {
+                const selection = event.detail.selection.value;
+                autoCompleteJS.input.value = selection;
+            },
+        },
+    },
+    resultsList: {
+        maxResults: 8,
+    },
+    searchEngine: 'strict', // strict | loose
+});
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
